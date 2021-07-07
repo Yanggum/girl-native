12883c5eb1de40c3676742e97c65c6bc9369b1db
Yanggum <bak405@naver.com>
2021-07-07 23:17:33 +0900

    20210707 Hello World Native

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert } from 'react-native';
+//import back from './images/sample_background.png';
+//image 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./images/sample_background.png')} style={styles.backgroundImage}/>
+          <View>
+            <Image onClick={ () => { alert("Hello World!")}} source={require('./images/sayaka_magical.png')} style={styles.characterImage, {
+                      inset: '0px',
+                      height: '100%',
+                      opacity: 1,
+                      position: 'fixed',
+                      width: '100%',
+                      zIndex: 3}}/>
+          </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  backgroundImage:{
+    flex: 1,
+    resizeMode: 'cover'
+  },
+  characterImage:{
+    flex: 1,
+    inset: '0px',
+    height: '100%',
+    opacity: 1,
+    position: 'fixed',
+    width: '100%',
+    zIndex: 3,
+  },
+  loginForm: {
+    position: 'center',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+},
 });
